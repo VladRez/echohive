955c062b7ec76827a9d3a11001cd5e0ebbae28c7
vladrez <vladrezdev@gmail.com>
2019-08-13 11:06:48 -0700

    front end axios user registration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
+import axios from 'axios';
 
 import Root from './components/root';
 import configureStore from './store/store';
@@ -11,9 +12,10 @@ import { logout } from './actions/session_actions';
 // import * as serviceWorker from './serviceWorker';
 
 
-
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    window.axios = axios;
+debugger
+});
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
