b2a35c185547b303ff34ddf4cf2e24d7846ced44
vladrez <vladrezdev@gmail.com>
2019-08-13 22:51:04 -0700

    refactor modal css

--- frontend/src/components/modal/session_form_modal.jsx
+++ frontend/src/components/modal/session_form_modal.jsx
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import LoginFormContainer from '../session/login_form_container';
 import SignupFormContainer from '../session/signup_form_container';
 
+import './session_modal.css'
+
 function Modal({modal, closeModal}) {
     if (!modal) {
       return null;
