e56e3c6c70b4eb7eb725769fd84184f44f9a6f65
vladrez <vladrezdev@gmail.com>
2019-08-13 21:51:35 -0700

    session page styling

--- frontend/src/components/session/session.jsx
+++ frontend/src/components/session/session.jsx
@@ -1,13 +1,33 @@
 import React from "react";
-
+import "./session.css";
 const Session = props => {
-  
   const sessionLinks = () => (
-    <nav className="longin-signup">
-      <button onClick={() => props.openModal("login")}>Login</button>
-    
-      <button onClick={() => props.openModal("signup")}>Signup</button>
-    </nav>
+    <div className="hero-container">
+      <div className="hero-inner">
+        <div className="hero-front">
+          <div className="hero-front-container">
+            <span className="hero-project-logo" />
+            <div className="hero-login-signin">
+              <button
+                className="hero-login-button"
+                onClick={() => props.openModal("login")}
+              >
+                Login
+              </button>
+              <button
+                className="hero-signup-button"
+                onClick={() => props.openModal("signup")}
+              >
+                Signup
+              </button>
+              <a href="#" className="hero-demo">
+                Demo
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
   const welcomePage = () => <h1>Logged in!</h1>;
   return props.isAuthenticated ? welcomePage() : sessionLinks();
