7499c4a3c1e3f0b04f706fe5bc90d7d7922ee4e8
reynier25 <fsoto@uchicago.edu>
2019-08-15 15:03:12 -0700

    fetchUserTracks for user profile

--- frontend/src/util/track_api_util.js
+++ frontend/src/util/track_api_util.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const getTracks = (id) => {
-    return axios.get('/api/tracks/', id)
+export const getTracks = () => {
+    return axios.get('/api/tracks/')
 };
 
 export const getSingleTrack = id => {
