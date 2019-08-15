43e75bcc536cb86ab9dcfb7a7993ca6e0653caa1
vladrez <vladrezdev@gmail.com>
2019-08-14 23:42:42 -0700

    stable nav

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import './navbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -10,47 +10,52 @@ class NavBar extends React.Component {
   }
 
   logoutUser(e) {
-      e.preventDefault();
-      this.props.logout();
+    e.preventDefault();
+    this.props.logout();
   }
 
   // Selectively render links dependent on whether the user is logged in
   getLinks() {
-    
-      if (this.props.loggedIn) {
-        return (
-            <div>
-                <button onClick={this.logoutUser}>Logout</button>
-            </div>
-        );
-      }
+    if (this.props.loggedIn) {
+      return (
+        <div>
+          <button onClick={this.logoutUser}>Logout</button>
+        </div>
+      );
+    }
   }
 
   headerNavBar() {
+    
     return (
       <header className="site-nav-header">
-            <div className="site-nav-contents">
-              <div className="nav-header-left">
-                <div className="header-nav-logo">
-                  <a href="/"></a>
-                </div>
-              </div>
-              <div className="nav-header-middle"></div>
-              <div className="nav-header-right">
-
-              </div>
+        <div className="site-nav-contents">
+          <div className="nav-header-left">
+            <div className="header-nav-logo">
+              <a href="/" />
             </div>
-    
-          {/* {this.getLinks()} */}
+          </div>
+          <div className="nav-header-middle" />
+          <div className="nav-header-right" >
+          <div className="header-nav-button">
+              <a href="#/upload" >Upload</a>
+            </div>
+            <div className="header-nav-button">
+              <a href="/" onClick={this.logoutUser}>Logout</a>
+            </div>
+            <div className="header-nav-button">
+              <a href={`#/user/${this.props.currentUser.id}`} >{this.props.currentUser.username}</a>
+            </div>
+          </div>
+        </div>
+
+        {/* {this.getLinks()} */}
       </header>
     );
-    
   }
 
   render() {
-      return (
-        this.props.loggedIn ? this.headerNavBar() : ''
-      );
+    return this.props.loggedIn ? this.headerNavBar() : "";
   }
 }
 
