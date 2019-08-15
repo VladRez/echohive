fd64a07591568fb1a6a0a8208e88e01207731d31
vladrez <vladrezdev@gmail.com>
2019-08-15 12:37:01 -0700

    find user by id api

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,8 +2,6 @@ import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch, Route } from "react-router-dom";
 import React from "react";
 import MainPage from "./main/main_page";
-import LoginFormContainer from "./session/login_form_container";
-import SignupFormContainer from "./session/signup_form_container";
 import TrackComposeContainer from "./tracks/track_compose_container";
 import TrackShowContainer from "./tracks/track_show_container";
 import TracksContainer from "./tracks/tracks_container";
@@ -14,20 +12,21 @@ import SessionModal from "./modal/session_form_modal";
 const App = () => (
   <div>
     <SessionModal />
-        <NavBarContainer/>
+    <NavBarContainer />
     <Switch>
       <AuthRoute exact path="/" component={SessionPage} />
-      {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
-          <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
-      <ProtectedRoute path="/feed" component={MainPage} />
+      <ProtectedRoute exact path="/feed" component={TracksContainer} />
+      <ProtectedRoute
+        exact
+        path="/tracks/upload"
+        component={TrackComposeContainer}
+      />
+      <ProtectedRoute
+        exact
+        path="/tracks/:trackId"
+        component={TrackShowContainer}
+      />
     </Switch>
-    <ProtectedRoute
-      exact
-      path="/tracks/upload"
-      component={TrackComposeContainer}
-    />
-    <Route exact path="/tracks/:trackId" component={TrackShowContainer} />
-    <Route exact path="/feed" component={TracksContainer} />
   </div>
 );
 
