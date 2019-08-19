0ddbe374c57ebd3949971e98a0d4b78329423169
vladrez <vladrezdev@gmail.com>
2019-08-19 09:46:21 -0700

    add nav player presentation and container

---
+++ frontend/src/components/nav_player/nav_player.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./nav_player.css";
+
+class NavPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>player</div>;
+  }
+}
+
+export default NavPlayer;
