5d696629b4050167c9612c7d7b5601671771db93
vladrez <vladrezdev@gmail.com>
2019-08-19 09:38:09 -0700

    add player nav container

---
+++ frontend/src/components/nav_player/nav_player_container.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import NavPlayer from "./nav_player";
+const mapStateToProps = (state, ownProps) => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NavPlayer);
